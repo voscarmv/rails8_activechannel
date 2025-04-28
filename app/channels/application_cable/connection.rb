8c4f686b0230e3f53aacdba2923b51ac05f036d8
Oscar Mier <voscarmv@gmail.com>
2025-04-28 22:16:17 +0000

    First connection success

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -7,10 +7,13 @@ module ApplicationCable
     end
 
     private
-      def set_current_user
-        if session = Session.find_by(token: request.headers['Sec-WebSocket-Protocol']&.split(" ")[-1])
-          self.current_user = session.user
-        end
+    def set_current_user
+      # puts request.params.inspect
+      # puts request.headers.inspect
+      # if session = Session.find_by(token: request.headers['Sec-WebSocket-Protocol']&.split(",")[-1])
+      if session = Session.find_by(token: request.params[:token])
+        self.current_user = session.user
       end
+    end
   end
 end
